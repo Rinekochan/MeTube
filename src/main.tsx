517487ebc6d92e177163b084d85e3fdc724ec5dc
Viet Hoang Pham <104506968@student.swin.edu.au>
2025-07-04 23:33:14 +1000

    Set up Vite React project

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider } from "@mui/material";
+import theme from "./styles/theme.ts";
+import "./App.css";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+);
